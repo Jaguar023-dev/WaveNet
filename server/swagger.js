934dbc084b856093fd378c419e6a433445ad2536
Jaguar023-dev <derrickithicia8@gmail.com>
2025-12-22 09:44:40 +0300

    Create swagger.js

---
+++ server/swagger.js
@@ -0,0 +1,42 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'WaveNet API',
+      version: '1.0.0',
+      description: 'Social Media Platform API Documentation',
+      contact: {
+        name: 'WaveNet Support',
+        email: 'support@wavenet.com'
+      }
+    },
+    servers: [
+      {
+        url: process.env.API_URL || 'http://localhost:5000/api',
+        description: 'Development server'
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    },
+    security: [{
+      bearerAuth: []
+    }]
+  },
+  apis: ['./routes/*.js']
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
