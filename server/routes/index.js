678d54d950dc1bb5447089be174d69c123687d22
Jaguar023-dev <derrickithicia8@gmail.com>
2025-12-22 09:39:30 +0300

    Create index.js

---
+++ server/routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+// Import all routes
+const authRoutes = require('./auth');
+const userRoutes = require('./users');
+const postRoutes = require('./posts');
+const groupRoutes = require('./groups');
+const messageRoutes = require('./messages');
+const notificationRoutes = require('./notifications');
+const storyRoutes = require('./stories');
+const marketplaceRoutes = require('./marketplace');
+const eventRoutes = require('./events');
+const videoRoutes = require('./videos');
+const pageRoutes = require('./pages');
+
+// Use routes
+router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/groups', groupRoutes);
+router.use('/messages', messageRoutes);
+router.use('/notifications', notificationRoutes);
+router.use('/stories', storyRoutes);
+router.use('/marketplace', marketplaceRoutes);
+router.use('/events', eventRoutes);
+router.use('/videos', videoRoutes);
+router.use('/pages', pageRoutes);
+
+module.exports = router;
