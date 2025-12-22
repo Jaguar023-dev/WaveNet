97bc8e9c7474495ffe61ad45d95443970d20783b
Jaguar023-dev <derrickithicia8@gmail.com>
2025-12-22 15:22:44 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
-import './index.css'; // if you have this
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
