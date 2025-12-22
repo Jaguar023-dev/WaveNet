24c8ffb758b0d2ae0e62b6bb7f8cbc16e002b0a1
Jaguar023-dev <derrickithicia8@gmail.com>
2025-12-22 09:14:03 +0300

    Create src/App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Layout Components
+import Layout from './components/Layout/Layout';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
+
+// Pages
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import Profile from './pages/Profile/Profile';
+import Friends from './pages/Friends/Friends';
+import Groups from './pages/Groups/Groups';
+import Watch from './pages/Watch/Watch';
+import Marketplace from './pages/Marketplace/Marketplace';
+import Messenger from './pages/Messenger/Messenger';
+import Notifications from './pages/Notifications/Notifications';
+import Settings from './pages/Settings/Settings';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route path="/friends" element={<Friends />} />
+              <Route path="/groups" element={<Groups />} />
+              <Route path="/watch" element={<Watch />} />
+              <Route path="/marketplace" element={<Marketplace />} />
+              <Route path="/messenger" element={<Messenger />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+          </Route>
+        </Routes>
+        <ToastContainer position="top-right" autoClose={3000} />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
