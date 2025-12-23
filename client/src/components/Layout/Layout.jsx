1560b41077356e7d3680b7bfeda8dcd7cfd56c40
Jaguar023-dev <derrickithicia8@gmail.com>
2025-12-23 21:32:32 +0300

    Update Layout.jsx

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,7 +1,9 @@
+// client/src/components/Layout/Layout.jsx - UPDATED
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import Sidebar from './Sidebar';
+import Sidebar from './Sidebar'; // Left sidebar
+import RightSidebar from './RightSidebar'; // Right sidebar
 import './Layout.css';
 
 const Layout = () => {
@@ -9,10 +11,11 @@ const Layout = () => {
     <div className="layout">
       <Header />
       <div className="layout-container">
-        <Sidebar />
+        <Sidebar /> {/* Left sidebar */}
         <main className="main-content">
-          <Outlet /> {/* This renders the child routes */}
+          <Outlet /> {/* This renders Home.jsx content */}
         </main>
+        <RightSidebar /> {/* Right sidebar */}
       </div>
     </div>
   );
